41083f1806cfbc035268e5ff3f975a4c615c00bd
Airbus5717 <Abdussamadf350@gmail.com>
2021-10-18 20:21:27 +0300

    fix test

--- hcat/test/main.c
+++ hcat/test/main.c
@@ -1,17 +1,9 @@
+#include <stdio.h>
+// comment test
+int main(void)
+{
+    char x = 's';
+    printf("Hello World");
+    return 0;
+}
 
-#include <stdio.h>
-// comment test
-int main(void)
-{
-    char x = 's';
-    printf("Hello World");
-    return 0;
-}
-#include <stdio.h>
-// comment test
-int main(void)
-{
-    char x = 's';
-    printf("Hello World");
-    return 0;
-}
